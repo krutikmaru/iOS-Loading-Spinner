3a04bc0a22abb9df4c5778e47836de1d06e78bae
Krutik <134735761+krutikmaru@users.noreply.github.com>
2023-06-26 14:11:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styles.css";
+
+const IOSLoadingSpinner = () => {
+  return (
+    <div className="spinner-container">
+      <div className="spinner">
+        <div className="spinner-line"></div>
+        <div className="spinner-line"></div>
+        <div className="spinner-line"></div>
+        <div className="spinner-line"></div>
+        <div className="spinner-line"></div>
+        <div className="spinner-line"></div>
+        <div className="spinner-line"></div>
+        <div className="spinner-line"></div>
+      </div>
+    </div>
+  );
+};
+
+export default IOSLoadingSpinner;
